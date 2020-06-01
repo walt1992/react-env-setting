3e6b5698270509cbe6f0b5b8259719644eec8edd
reallifeliver <pms3300@gmail.com>
2020-06-01 20:05:07 +0900

    eslint & prettier

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 const Todo = () => {
-    return (
-        <div>
-            <input/>
-            <button>등록</button>
-            <button>삭제</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <input />
+      <button>등록</button>
+      <button>삭제</button>
+    </div>
+  );
+};
+
 export default Todo;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import ReactDom from "react-dom";
+import * as React from 'react';
+import ReactDom from 'react-dom';
 import Todo from './components/Todo';
 import './styles/app.scss';
 
 const wrapper = document.getElementById('container');
-wrapper ? ReactDom.render(<Todo/>, wrapper) : false;
+wrapper ? ReactDom.render(<Todo />, wrapper) : false;
